37cecb6be68fa2c643af5cfb6cafe70c62d21570
Arialdo Martini <arialdomartini@gmail.com>
2014-01-15 18:43:52 +0100

    fabric can start and stop a python web app served by gunicorn on a fixed remote server.
    The web app must be already present in the user's remote home, in a virtualenv called appenv.
    The virtualenv must already have flask and gunicorn installed.

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import env, task, run
+from fabric.context_managers import prefix
+
+env.use_ssh_config=True
+
+env.hosts = ['medmed']
+gunicorn_pid = 'gunicorn_pid'
+port=8001
+
+def activate():
+    return 'cd {appenv} && source bin/activate'
+
+@task
+def start():
+    with prefix(activate()):
+        print "Starting the app"
+        run('gunicorn --daemon -p {0} -b 0.0.0.0:{1} app:app'.format(gunicorn_pid, port))
+
+@task
+def stop():
+    with prefix(activate()):
+        print "Stopping the app"
+        run('kill -9 `cat {0}`'.format(gunicorn_pid))
+        run('rm {0}'.format(gunicorn_pid))
