9f3b4e448bd3cbb3678f69026e47c6e5a4a21c78
Arialdo Martini <arialdomartini@gmail.com>
2014-01-20 23:15:38 +0100

    fabric can start and stop 2 web-app served by gunicorn on a fixed remote server
    Each web-app must be already deployed on the remote server.
    
    Arialdo can start his app with `fab start:ari` and `fab start:ari`.
    
    Mattia can start his app with `fab start:mattia` and `fab start:mattia`.
    
    The 2 web-apps will use different http ports, so they could be deployed independentely.

--- fabfile.py
+++ fabfile.py
@@ -6,21 +6,38 @@ env.use_ssh_config=True
 env.hosts = ['medmed']
 gunicorn_pid = 'gunicorn_pid'
 port=8001
-appenv='appenv'
+
+contexts = {
+    'mattia': {
+        'port': 8001,
+        'virtualenv': 'appenv'
+    },
+    'ari': {
+        'port': 8002,
+        'virtualenv': 'appenv'
+    }
+}
+
+def activate(context_name):
+    return 'cd {0} && source bin/activate'.format(context_name)
 
 
-def activate():
-    return 'cd {0} && source bin/activate'.format(appenv)
+def get_context(context_name):
+    if not context_name in contexts:
+        abort("No context named {0}".format(context_name))
+    return contexts[context_name] 
 
 @task
-def start():
-    with prefix(activate()):
-        print "Starting the app"
+def start(context_name):
+    context = get_context(context_name)
+    with prefix(activate(context['virtualenv'])):
+        print "Starting the app on context ".format(context_name)
         run('gunicorn --daemon -p {0} -b 0.0.0.0:{1} app:app'.format(gunicorn_pid, port))
 
 @task
-def stop():
-    with prefix(activate()):
-        print "Stopping the app"
+def stop(context_name):
+    context = get_context(context_name)
+    with prefix(activate(context['virtualenv'])):
+        print "Stopping the app on context ".format(context_name)
         run('kill -9 `cat {0}`'.format(gunicorn_pid))
         run('rm {0}'.format(gunicorn_pid))
